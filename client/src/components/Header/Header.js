450f832fcf9d627c306ac13263cce5676f3b38c8
Dimitar Sergev <102225734+DimitarSergeev@users.noreply.github.com>
2022-07-21 15:35:42 +0300

    create Nav component and css

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,58 @@
+import styles from './Header.module.css'
+
+import { NavLink } from 'react-router-dom'
+
+export const Header = () => {
+
+    return (
+        <div className={styles.navigation}>
+            
+                <NavLink to='/'
+                    
+                >
+                    <img src="images/logo.jpg" alt="" className={styles.logo} />
+                    
+                </NavLink>
+            
+            <ul>
+                <li className={styles.list}>
+                    <NavLink to='/'
+                        className={({ isActive }) => (isActive ? styles.active : styles.text)}
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li className={styles.list}>
+                    <NavLink to='/auction'
+                        className={({ isActive }) => (isActive ? styles.active : styles.text)}
+                    >
+                        Auction
+                    </NavLink>
+                </li>
+                <li className={styles.list}>
+                    <NavLink to='/about'
+                        className={({ isActive }) => (isActive ? styles.active : styles.text)}
+                    >
+                        About
+                    </NavLink>
+                </li>
+                <li className={styles.list}>
+                    <NavLink to='/auth/login'
+                        className={({ isActive }) => (isActive ? styles.active : styles.text)}
+                    >
+                        Login
+                    </NavLink>
+                </li>
+                <li className={styles.list}>
+                    <NavLink to='/auth/register'
+                        className={({ isActive }) => (isActive ? styles.active : styles.text)}
+                    >
+                        Register
+                    </NavLink>
+                </li>
+
+            </ul>
+        </div>
+    )
+
+}
