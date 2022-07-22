335af0afcad03d1abd90028fe9bb763a5361dc91
Dimitar Sergev <102225734+DimitarSergeev@users.noreply.github.com>
2022-07-22 16:38:37 +0300

    add cover photo to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 import {Header} from './components/Header/Header'
+import { Footer} from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <main className='main'>
+        {/* <img src="/images/atm.jpg" alt="" className='backGroundImg'/> */}
+        {/* <h2>asdasd</h2> */}
+      </main>
+      <Footer />
     </div>
   );
 }
